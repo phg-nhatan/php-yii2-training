b9285abeeadd5c0706a91c14e890a1ebb01f1510
Tan Phan <tanphan250102@gmail.com>
2025-09-08 19:08:47 +0700

    chore(project): initialize project structure

---
+++ src/config/db.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;dbname=yii2db',
+    'username' => 'yii2',
+    'password' => 'yii2pass',
+    'charset' => 'utf8',
+    // 'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    // 'username' => 'root',
+    // 'password' => '',
+    // 'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
