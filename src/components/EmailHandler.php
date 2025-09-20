062f586eebf6230ebefccb7d0d8469dc0518f47c
Tan Phan <tanphan250102@gmail.com>
2025-09-20 10:32:11 +0700

    [issue_001] Create EmailHandler

---
+++ src/components/EmailHandler.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link https://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license https://www.yiiframework.com/license/
+ */
+
+namespace app\components;
+
+/**
+ * @author Tan Phan <tanphan250102@gmail.com>
+ * @since 2.0
+ */
+class EmailHandler {
+
+}
